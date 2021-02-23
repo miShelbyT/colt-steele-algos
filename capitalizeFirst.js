a43d67caf57bd4041d33dee51ddd4b2088901c32
miShelbyT <mi.shelbyrose@gmail.com>
2021-02-22 23:22:19 -0500

    successgit add . on a slightly more complex recursive prob

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,19 +1,15 @@
 function capitalizeFirst(array) {
   let newArray = []
 
-  function helper(array) {
-    if (array.length === 0) return;
+    if (array.length === 0) return newArray;
+
     let newWord = array.slice(0, 1)
     newWord = newWord.toString().charAt().toUpperCase() + newWord.toString().slice(1)
     // console.log(newWord)
     // console.log(typeof newWord)
     newArray.push(newWord)
     // console.log(newArray)
-    helper(array.slice(1))
-  }
-  helper()
-
-  return newArray;
+  return newArray.concat(capitalizeFirst(array.slice(1)));
 }
 
 console.log(capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
