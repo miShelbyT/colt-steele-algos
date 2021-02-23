6d61a18e5460fa64b817b06103fc78fe4c17b20d
miShelbyT <mi.shelbyrose@gmail.com>
2021-02-23 11:55:10 -0500

    solved capitalizeFirst with helper and also with pure recursion. super cool

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,16 +1,43 @@
+// WITH HELPER
 function capitalizeFirst(array) {
   let newArray = []
 
-    if (array.length === 0) return newArray;
+  function helper(array) {
+    if (array.length === 0) return;
 
-    let newWord = array.slice(0, 1)
-    newWord = newWord.toString().charAt().toUpperCase() + newWord.toString().slice(1)
+    let newWord = array[0]
     // console.log(newWord)
-    // console.log(typeof newWord)
+    newWord = newWord.charAt().toUpperCase() + newWord.slice(1)
+    // console.log(newWord)
     newArray.push(newWord)
-    // console.log(newArray)
-  return newArray.concat(capitalizeFirst(array.slice(1)));
+    helper(array.slice(1))
+    // console.log(newArray)  
+  }
+
+  helper(array)
+  return newArray
 }
+
+
+// .join() and .toString() are iterative and take more time complexity O(N)
+// using .slice() or array[0] is O(1)
+
+
+// PURE RECURSION
+// function capitalizeFirst(array) {
+//   let newArray = []
+
+//     if (array.length === 0) return newArray;
+
+//     let newWord = array.slice(0, 1)
+//     newWord = newWord.toString().charAt().toUpperCase() + newWord.toString().slice(1)
+//     // console.log(newWord)
+//     // console.log(typeof newWord)
+//     newArray.push(newWord)
+//     // console.log(newArray)
+//     // .concat() carries the values from the previous iterations so we don't overwrite with .push()
+//   return newArray.concat(capitalizeFirst(array.slice(1)));
+// }
 
 console.log(capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
 
